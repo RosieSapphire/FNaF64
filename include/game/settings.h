56bc878c33a5081a8694f5909ac459018d3a0e95
Rosie Sapphire <jayjaygames40@gmail.com>
2024-09-22 21:46:41 -0700

    Abstracted settings out to its own files as well

--- include/game/settings.h
+++ include/game/settings.h
@@ -2,14 +2,16 @@
 #define GAME_SETTINGS_H
 
 extern int settings_flags;
-#define SET_SMOOTH_TURN_BIT 1
-#define SET_MANUAL_CAM_TURN_BIT 2
-#define SET_SUBTITLES_BIT 4
-#define SET_LIGHT_HOLD_BIT 8
-#define SET_ROBOT_CHEAT_BIT 16
-#define SET_GOOFY_MODE_BIT 32
-#define SET_FAST_NIGHT_BIT 64
-#define SETTING_COUNT 7
+enum {
+	SET_SMOOTH_TURN_BIT = (1 << 0),
+	SET_MANUAL_CAM_TURN_BIT = (1 << 1),
+	SET_SUBTITLES_BIT = (1 << 2),
+	SET_LIGHT_HOLD_BIT = (1 << 3),
+	SET_ROBOT_CHEAT_BIT = (1 << 4),
+	SET_GOOFY_MODE_BIT = (1 << 5),
+	SET_FAST_NIGHT_BIT = (1 << 6),
+	SETTING_COUNT = 7,
+};
 
 #define HOUR_LEN_SECONDS ((settings_flags & SET_FAST_NIGHT_BIT) ? 45 : 90)
 
---
+++ src/game/settings.c
@@ -0,0 +1,3 @@
+#include "game/settings.h"
+
+int settings_flags = 0; //SET_ROBOT_CHEAT_BIT;
