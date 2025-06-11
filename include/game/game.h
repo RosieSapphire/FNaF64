369fe25ebef7d52e890a020217cabb7fb47dfa0e
Rosie <jayjaygames40@gmail.com>
2025-06-10 21:12:09 -0500

    Whoops, forgot to include the jumpscare flags in the header file in the last commit. lmfao

--- include/game/game.h
+++ include/game/game.h
@@ -7,10 +7,27 @@
 #include "engine/scene.h"
 #include "engine/update.h"
 
+enum {
+        JUMPSCARE_FLAG_BONNIE_SHIFT,
+        JUMPSCARE_FLAG_CHICA_SHIFT,
+        JUMPSCARE_FLAG_FOXY_SHIFT,
+        JUMPSCARE_FLAG_FREDDY_SHIFT
+};
+
+enum {
+        JUMPSCARE_FLAG_BONNIE = (1 << JUMPSCARE_FLAG_BONNIE_SHIFT),
+        JUMPSCARE_FLAG_CHICA  = (1 << JUMPSCARE_FLAG_CHICA_SHIFT),
+        JUMPSCARE_FLAG_FOXY   = (1 << JUMPSCARE_FLAG_FOXY_SHIFT),
+        JUMPSCARE_FLAG_FREDDY = (1 << JUMPSCARE_FLAG_FREDDY_SHIFT),
+        JUMPSCARE_FLAG_MASK   = JUMPSCARE_FLAG_BONNIE | JUMPSCARE_FLAG_CHICA |
+                                JUMPSCARE_FLAG_FOXY | JUMPSCARE_FLAG_FREDDY
+};
+
 extern float game_night_timer;
 extern int game_power_usage;
 extern int game_power_left;
 extern float game_power_timer;
+extern uint8_t game_jumpscare_flags;
 
 void game_draw(void);
 enum scene game_update(struct update_params uparms);
