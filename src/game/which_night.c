a8cc83bab5154e4530a798a73933bf59ffde8c03
Rosie <jayjaygames40@gmail.com>
2025-06-12 03:33:01 -0500

    Cleaned up `src/game/which_night.c`.

--- src/game/which_night.c
+++ src/game/which_night.c
@@ -11,11 +11,10 @@
 #include "game/save_data.h"
 #include "game/which_night.h"
 
-struct graphic which_night_atlas;
-static float timer;
-static bool is_loaded = false;
+#define WHICH_NIGHT_TIMER_FADEOUT (70.f / 60.f)
+#define WHICH_NIGHT_TIMER_END (130.f / 60.f)
 
-static const int night_ai_lut[6][4] = {
+static const int which_night_ai_lut[6][4] = {
 	{ 0,  0,  0,  0},
 	{ 3,  1,  1,  0},
 	{ 0,  5,  2,  1},
@@ -24,45 +23,60 @@ static const int night_ai_lut[6][4] = {
 	{10, 12,  6,  4},
 };
 
-static void _which_night_load(void)
+struct graphic which_night_gfx_atlas;
+
+static float which_night_timer;
+static bool which_night_is_loaded = false;
+
+static void which_night_load(void)
 {
-	if (is_loaded)
-		return;
-	timer = 0.0f;
-	graphic_load(&which_night_atlas, TX_WHICH_NIGHT_ATLAS);
-	is_loaded = true;
+        int night_cur;
+
+        /* Graphics */
+	graphic_load(&which_night_gfx_atlas, TX_WHICH_NIGHT_ATLAS);
+
 	blip_trigger(true);
+	which_night_is_loaded = true;
 
-	if (SAVE_NIGHT_NUM(save_data) == 7)
+        /* If we're doing custom night, don't change the AI. */
+        night_cur = SAVE_NIGHT_NUM(save_data);
+	if (night_cur == 7) {
 		return;
+        }
 
-	bonnie_ai_level = night_ai_lut[SAVE_NIGHT_NUM(save_data) - 1][0];
-	chica_ai_level = night_ai_lut[SAVE_NIGHT_NUM(save_data) - 1][1];
-	foxy_ai_level = night_ai_lut[SAVE_NIGHT_NUM(save_data) - 1][2];
-	freddy_ai_level = night_ai_lut[SAVE_NIGHT_NUM(save_data) - 1][3];
+        /* Variables */
+	which_night_timer = 0.f;
+	bonnie_ai_level   = which_night_ai_lut[night_cur - 1][0];
+	chica_ai_level    = which_night_ai_lut[night_cur - 1][1];
+	foxy_ai_level     = which_night_ai_lut[night_cur - 1][2];
+	freddy_ai_level   = which_night_ai_lut[night_cur - 1][3];
 }
 
-static void _which_night_unload(void)
+static void which_night_unload(void)
 {
-	if (!is_loaded)
-		return;
-	graphic_unload(&which_night_atlas);
-	is_loaded = false;
+	graphic_unload(&which_night_gfx_atlas);
+	which_night_is_loaded = false;
 }
 
 void which_night_draw(void)
 {
-	_which_night_load();
+        float fade_alpha;
+
+        if (!which_night_is_loaded) {
+	        which_night_load();
+        }
 
 	rdpq_set_mode_fill(RGBA32(0, 0, 0, 0xFF));
 	rdpq_fill_rectangle(0, 0, 320, 240);
 	rdpq_set_mode_standard();
-	graphic_draw_index_y(which_night_atlas, 372, 270, 11, 0, GFX_FLIP_NONE);
-	graphic_draw_index_y(which_night_atlas, 373, 336, 11,
+	graphic_draw_index_y(which_night_gfx_atlas, 372, 270,
+                             11, 0, GFX_FLIP_NONE);
+	graphic_draw_index_y(which_night_gfx_atlas, 373, 336, 11,
                              SAVE_NIGHT_NUM(save_data), GFX_FLIP_NONE);
 
-	float fade = CLAMP((timer - 70) / 60.0f, 0, 1);
-	rdpq_set_prim_color(RGBA32(0x0, 0x0, 0x0, fade * 255));
+        fade_alpha =
+                CLAMP((which_night_timer - WHICH_NIGHT_TIMER_FADEOUT), 0, 1);
+	rdpq_set_prim_color(RGBA32(0x0, 0x0, 0x0, fade_alpha * 0xFF));
 	rdpq_mode_combiner(RDPQ_COMBINER_FLAT);
 	rdpq_mode_blender(RDPQ_BLENDER_MULTIPLY);
 	rdpq_fill_rectangle(0, 0, 320, 240);
@@ -72,10 +86,10 @@ void which_night_draw(void)
 
 enum scene which_night_update(struct update_params uparms)
 {
-	timer += uparms.dt * 60;
+	which_night_timer += uparms.dt;
 
-	if (timer >= 130) {
-		rdpq_call_deferred((void (*)(void *))_which_night_unload, NULL);
+	if (which_night_timer >= WHICH_NIGHT_TIMER_END) {
+                which_night_unload();
 		return SCENE_MAIN_GAME;
 	}
 
