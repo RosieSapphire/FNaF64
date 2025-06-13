b5e2308b0f70fa5c343ca03642b2e87d8b308444
Rosie <jayjaygames40@gmail.com>
2025-06-13 00:25:15 -0500

    [REDO] Add make-with-docker shell script

---
+++ make-with-docker.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -f FiveNightsatFreddys.exe ]; then
+    echo "ERROR: FiveNightsatFreddys.exe nmust be in the project directory!"
+    exit 1
+fi
+
+if [ ! -d CTFAK2.0 ] || [ ! -d libdragon ]; then
+    git submodule update --init
+fi
+
+if [ "$(uname -m)" != "x86_64" ]; then
+    echo "WARNING: This project is only supported on x86_64 systems."
+    echo "         You may encounter issues building on other platforms!"
+fi
+
+if [ ! -d dump ]; then
+    # Build CTFAK.Cli
+    if [ ! -d CTFAK.Cli ]; then
+        docker build \
+            --file Dockerfile-ctfak \
+            --progress plain \
+            --platform linux/amd64 \
+            --target out \
+            --output type=local,dest=. \
+            .
+    fi
+
+    # Dump the game assets
+    docker build \
+        --file Dockerfile-dump \
+        --progress plain \
+        --platform linux/amd64 \
+        --target out \
+        --output type=local,dest=. \
+        .
+fi
+
+# Build the ROM
+docker build \
+    --file Dockerfile \
+    --progress plain \
+    --platform linux/amd64 \
+    --target out \
+    --output type=local,dest=. \
+    .
